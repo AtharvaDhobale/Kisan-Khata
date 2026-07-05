5a0a49ed8409832f17cec072045ddbffdb5c6e65
Farmoholic Dev <farmoholic@farmer.com>
2026-07-06 00:29:08 +0530

    chore: remove temp batch file

---
+++ g2.bat
@@ -0,0 +1,4 @@
+@echo off
+git add .
+git commit -m "chore: remove temp batch file"
+echo Done
--- gitcommit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git config user.email "farmoholic@farmer.com"
-git config user.name "Farmoholic Dev"
-git add .
-git commit -m "feat: initial Farmoholic release"
-echo DONE
